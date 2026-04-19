448fcccbd5e9a6f1943f69d4cc4b4df0699f8ac7
masxkip <jesufemiketiku@gmail.com>
2026-04-19 11:27:52 -0700

    new front up

--- javas/script.js
+++ javas/script.js
@@ -77,7 +77,7 @@ document.addEventListener("DOMContentLoaded", function () {
         
   
         try {
-            const response = await fetch("https://vgrill-contact.onrender.com/contact", {
+            const response = await fetch("https://contact-form-backend-cshp.onrender.com/contact", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(formData)
